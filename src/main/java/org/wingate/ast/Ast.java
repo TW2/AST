16ae69c94000c1d619b50b691125207aab8a4019
RedDog,csa <util2@msn.com>
2024-06-21 05:46:10 +0200

    init

---
+++ src/main/java/org/wingate/ast/Ast.java
@@ -0,0 +1,12 @@
+package org.wingate.ast;
+
+/**
+ *
+ * @author util2
+ */
+public class Ast {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
